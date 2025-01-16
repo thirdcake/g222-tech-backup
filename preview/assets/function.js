b1dfe8a0a24fdd4a6f026302b7820851a2ac2acb
thirdcake <ksktnk768@gmail.com>
2025-01-16 13:37:03 +0900

    modify css

--- preview/assets/function.js
+++ preview/assets/function.js
@@ -1,13 +1,4 @@
-function onButtonClick (event) {
-    if(event.target.closest('button')) {
-        const button = event.target.closest('button');
-        if( ! (button.dataset.onclickClass && button.dataset.target)) return;
-        document.querySelectorAll(button.dataset.target).forEach(target => {
-            target.classList.toggle(button.dataset.onclickClass);
-        });
-    }
-}
-
+"use strict";
 window.addEventListener('DOMContentLoaded', ()=>{
-    document.addEventListener('click', onButtonClick, false);
+    console.log('aaa');
 }, false);
